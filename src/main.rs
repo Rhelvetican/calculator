e98be30b75718cad8e4f57ec827fd5d685e70e4f
Rh4096 <rhelvetican@gmail.com>
2024-02-23 01:39:38 +0700

    hello_world

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{glib, Application};
+use gtk::{glib, Application, ApplicationWindow};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
@@ -7,6 +7,16 @@ fn main() -> glib::ExitCode {
     // Create a new application
     let app = Application::builder().application_id(APP_ID).build();
 
+    app.connect_activate(build_ui);
     // Run the application
     app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Hello, World!")
+        .build();
+
+    window.present();
 }
